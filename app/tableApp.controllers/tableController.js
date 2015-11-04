348ad7412b97a10ae526a40c019454a2df7b94d0
deepuke <dipuke@gmail.com>
2015-11-04 08:42:35 +0530

    Updated table ctrl

--- app/tableApp.controllers/tableController.js
+++ app/tableApp.controllers/tableController.js
@@ -6,44 +6,65 @@
 	function tableController() {
 		var _self = this;
 
-		this.addRow = function(id) {
-			_self.persons.splice(3, 0, {
-
-				flag : true,
-				details : {
-					id : id,
-					desc : 'desctipton 2',
-					remark : 'remark 2'
-				}
-			});
-		};
-		
-		this.removeRow = function(id){
-			_self.persons.splice(3, 1);
-		};
-
 		this.person = [];
 
+		this.calculate, this.toggleDetailsView;
+
+		_self.calculate = function() {
+			for (var i = 0; i < _self.persons.length; i++) {
+				if (_self.persons[i].selected && !checkItemExist('id_' + _self.persons[i].id)) {
+
+					var newObj = {
+						id : 'id_' + _self.persons[i].id,
+						showFlag : true,
+						toggleFlag : false,
+						details : {
+							desc : 'description ' + 'id_' + _self.persons[i].id,
+							remark : 'remark ' + 'id_' + _self.persons[i].id
+						}
+					};
+					_self.persons.splice((i + 1), 0, newObj);
+					_self.persons[i].showDropIcon = !_self.persons[i].showDropIcon;
+
+				}
+			}
+		};
+
+		function checkItemExist(id) {
+			var flag = false;
+			for (var i = 0; i < _self.persons.length; i++) {
+				if (_self.persons[i].id === id) {
+					flag = true;
+					break;
+				}
+			}
+			return flag;
+		}
+
+
+		this.toggleDetailsView = function(index) {
+			_self.persons[index + 1].toggleFlag = !_self.persons[index + 1].toggleFlag;
+		};
+
 		_self.persons = [{
-			flag : false,
-			id : 1,
+			selected : false,
+			showFlag : false,
+			showDropIcon : false,
+			id : '1',
 			name : 'Deepu',
 			age : '32'
 		}, {
-			flag : true,
-			details : {
-				id : 1,
-				desc : 'Description 1',
-				remark : 'Remark 1'
-			}
-		}, {
-			flag : false,
-			id : 2,
+			selected : false,
+			showFlag : false,
+			showDropIcon : false,
+			id : '2',
 			name : 'Anisha',
 			age : '25'
 		}, {
-			flag : false,
-			id : 3,
+			selected : false,
+			showFlag : false,
+			showDropIcon : false,
+			id : '3',
 			name : 'Chandrika',
 			age : '59'
 		}];
